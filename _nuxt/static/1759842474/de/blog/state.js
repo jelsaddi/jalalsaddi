b1d7c7c223f85c47a859db1f6b9759a29a63afc9
jelsaddi <jelsaddi@users.noreply.github.com>
2025-10-07 14:09:52 +0000

    Deploy resume website jelsaddi/jalal_el_saddi_resume@989d642f8327a0ff66e7969de0e293d9bfc6370c

--- _nuxt/static/1759842474/de/blog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fjalalsaddi\u002F_nuxt\u002Fstatic\u002F1759842474",layout:"default",error:a,serverRendered:b,routePath:"\u002Fde\u002Fblog",config:{_app:{basePath:"\u002Fjalalsaddi\u002F",assetsPath:"\u002Fjalalsaddi\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b}}}(null,true,"system"));
