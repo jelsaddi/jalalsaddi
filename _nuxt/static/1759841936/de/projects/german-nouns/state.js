46a72a047e1426111aa7f82c00dcd61f90cc9716
jelsaddi <jelsaddi@users.noreply.github.com>
2025-10-07 13:07:56 +0000

    Deploy resume website jelsaddi/jalal_el_saddi_resume@13f61637e984be459e3989a5fce42ace13ed910f

--- _nuxt/static/1759841936/de/projects/german-nouns/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1759841936",layout:"default",error:a,serverRendered:b,routePath:"\u002Fde\u002Fprojects\u002Fgerman-nouns",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b}}}(null,true,"system"));
