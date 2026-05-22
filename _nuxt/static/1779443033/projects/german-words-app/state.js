75d11e3f0083b11916904752fd254a310a0bbd21
jelsaddi <jelsaddi@users.noreply.github.com>
2026-05-22 09:50:58 +0000

    Deploy resume website jelsaddi/jalal_el_saddi_resume@9ae1d12080469a29a4d459d6ee8d7c1d6403f906

--- _nuxt/static/1779443033/projects/german-words-app/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1779443033",layout:"blog_layout",error:a,serverRendered:b,routePath:"\u002Fprojects\u002Fgerman-words-app",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b}}}(null,true,"system"));
